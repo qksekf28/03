b8176ba83ebfa79d42d63de6c33c02396c966bde
qksekf28 <hayoung902847@gmail.com>
2023-09-21 16:54:24 +0900

    4

--- main.c
+++ main.c
@@ -5,12 +5,16 @@
 
 int main(int argc, char *argv[])
 {
-	char c;
+	float a;
+	float b;
 	
-	printf("enter a character : ");
-	scanf("%c", &c);
+	printf("enter a numerator : ");
+	scanf("%f", &a);
 	
-	printf("The next character of %c (%d) is %c (%d)\n", c, c, c+1, c+1);
+	printf("enter a denominator : ");
+	scanf("%f", &b);
+	
+	printf("The result of devision ( %f / %f ) is %f\n", a, b, (float)a/b);
 	
 	system("PAUSE");
 	return 0;
